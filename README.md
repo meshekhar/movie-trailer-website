cd8aad27657d2fbfc39e1763405de558eb47df14
ravi shekhar <ravi@ognition.com>
2017-03-26 09:37:59 -0500

    added more description

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# Movie Trailer Site Application
-App contains server-side code to store a list of movies, box art imagery and a movie trailer URL. 
-Then serve this movie data as a web page allowing visitors to review their movies and watch the trailers.
+# Movie Trailer Website Application
+The Movie Trailer Website project consists of server-side code to store a list of movies titles, along with its respective box art imagery and movie trailer website. 
+The data should be served as a web page allowing visitors to review the movies and watch the trailers:
 
-Module fresh_tomatoes has a function called open_movies_page that takes in one argument, which is a list of movies 
-and creates an HTML file which visualizes all of your favorite movies.
+
+## How to run?
+
+* Install [Python](https://www.python.org/)
+* Verify that Python installed correctly by simply typing python at the command line. You should be greeted by the Python Shell.
+* Download the project files
+* Add your favorite movies to the entertainment_center module as instance of the Movie Class, providing movie title, short storyline, poster image url, and youtube trailer url.
+
+* Save entertainment_center module and run.
+* Movies website should open in the browser. Click on any one of the links, its trailer will play.
+
+## Modules
+* fresh_tomatoes module generates the website displaying movies listed in the entertainment_center module.
+* It has a function called open_movies_page that takes in one argument, which is a list of movies and creates an HTML file which visualizes all of your favorite movies.
+
+
+
+
+
--- entertainment_center.py
+++ entertainment_center.py
@@ -8,31 +8,46 @@ toy_story = media.Movie("Toy Story",
                         "https://www.youtube.com/watch?v=LKTU4AarZ7A")
                                                 
 power_rangers = media.Movie("Power Rangers", 
-                        "A group of high-school kids, who are infused with unique superpowers, harness their abilities in order to save the world.",
+                        "A group of high-school kids, who are infused with "
+                        "unique superpowers, harness their abilities in order"
+                        " to save the world.",
                         "https://upload.wikimedia.org/wikipedia/en/7/78/Power_Rangers_%282017_Official_Theatrical_Poster%29.png", 
                         "https://www.youtube.com/watch?v=5kIe6UZHSXw")
 
 beauty_and_the_beast = media.Movie("Beauty and the Beast", 
-                        "Belle (Emma Watson), a bright, beautiful and independent young woman, is taken prisoner by a beast (Dan Stevens) in its castle. ",
+                        "Belle (Emma Watson), a bright, beautiful and "
+                        "independent young woman, is taken prisoner by "
+                        "a beast (Dan Stevens) in its castle. ",
                         "https://upload.wikimedia.org/wikipedia/en/d/d6/Beauty_and_the_Beast_2017_poster.jpg", 
                         "https://www.youtube.com/watch?v=omEtQlhhM64")
 
 the_boss_baby = media.Movie("The boss baby", 
-                        "A new baby's arrival impacts a family, told from the point of view of a delightfully unreliable narrator -- a wildly imaginative 7-year-old named Tim.",
+                        "A new baby's arrival impacts a family, told from the"
+                        " point of view of a delightfully unreliable narrator "
+                        "-- a wildly imaginative 7-year-old named Tim.",
                         "https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/The_Boss_Baby_poster.jpg/220px-The_Boss_Baby_poster.jpg", 
                         "https://www.youtube.com/watch?v=tquIfapGVqs")
 
 the_matrix = media.Movie("The Matrix", 
-                        "The Matrix is a 1999 science fiction film written and directed by The Wachowski Brothers, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano.",
+                        "The Matrix is a 1999 science fiction film written "
+                        "and directed by The Wachowski Brothers, starring "
+                        "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, "
+                        "Hugo Weaving, and Joe Pantoliano.",
                         "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg", 
                         "https://www.youtube.com/watch?v=cqxwtEdxOCw")
 
 lord_of_the_rings = media.Movie("The Lord of the Rings", 
-                        "Journey to Middle-earth via the official The Lord of the Rings website. Get the insider scoop on the latest news, products and new content. Access videos ...",
+                        "Journey to Middle-earth via the official The Lord of "
+                        "the Rings website. Get the insider scoop on the latest"
+                        " news, products and new content",
                         "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif/220px-First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif", 
                         "https://www.youtube.com/watch?v=Pki6jbSbXIY")
 
-movies = [toy_story, power_rangers, beauty_and_the_beast, the_boss_baby, the_matrix, lord_of_the_rings]
+# Create movies array
+movies = [toy_story, power_rangers, beauty_and_the_beast, 
+          the_boss_baby, the_matrix, lord_of_the_rings]
+
+# Creates random shuffling of the movie array variable
 random.shuffle(movies)
 fresh_tomatoes.open_movies_page(movies)
 
